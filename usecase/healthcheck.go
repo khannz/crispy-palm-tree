ca9350889b338f0f6a760720cdc09301a9707e64
Ivan Tihonov <4thevan@gmail.com>
2020-04-25 18:45:57 +0300

    write self scheduler update state in storages

--- usecase/healthcheck.go
+++ usecase/healthcheck.go
@@ -3,9 +3,11 @@ package usecase
 // TODO: healthchecks != usecase!
 import (
 	"net"
+	"os"
 	"time"
 
 	"github.com/khannz/crispy-palm-tree/domain"
+	"github.com/khannz/crispy-palm-tree/portadapter"
 	"github.com/sirupsen/logrus"
 )
 
@@ -14,53 +16,144 @@ const healthcheckUUID = "00000000-0000-0000-0000-000000000004"
 
 // HeathcheckEntity ...
 type HeathcheckEntity struct {
-	locker           *domain.Locker
-	configuratorVRRP domain.ServiceWorker
-	logging          *logrus.Logger
+	cacheStorage      *portadapter.StorageEntity // so dirty
+	persistentStorage *portadapter.StorageEntity // so dirty
+	configuratorVRRP  domain.ServiceWorker
+	locker            *domain.Locker
+	gracefullShutdown *domain.GracefullShutdown
+	signalChan        chan os.Signal
+	waitRetry         chan struct{}
+	logging           *logrus.Logger
 }
 
 // NewHeathcheckEntity ...
-func NewHeathcheckEntity(locker *domain.Locker,
+func NewHeathcheckEntity(cacheStorage *portadapter.StorageEntity,
+	persistentStorage *portadapter.StorageEntity,
 	configuratorVRRP domain.ServiceWorker,
+	locker *domain.Locker,
+	gracefullShutdown *domain.GracefullShutdown,
+	signalChan chan os.Signal,
 	logging *logrus.Logger) *HeathcheckEntity {
 	return &HeathcheckEntity{
-		locker:           locker,
-		configuratorVRRP: configuratorVRRP,
-		logging:          logging,
+		cacheStorage:      cacheStorage,
+		persistentStorage: persistentStorage,
+		configuratorVRRP:  configuratorVRRP,
+		locker:            locker,
+		gracefullShutdown: gracefullShutdown,
+		signalChan:        signalChan,
+		waitRetry:         make(chan struct{}),
+		logging:           logging,
 	}
 }
 
-// CheckApplicationServersInServices ...
-func (hc *HeathcheckEntity) CheckApplicationServersInServices(servicesInfo []*domain.ServiceInfo) {
+// StartHealthchecks ...
+func (hc *HeathcheckEntity) StartHealthchecks(checkTime time.Duration) {
+	ticker := time.NewTicker(checkTime)
+	for {
+		select {
+		case <-ticker.C:
+			hc.tryHC()
+		case <-hc.waitRetry:
+			hc.tryHC()
+		}
+	}
+}
+
+func (hc *HeathcheckEntity) tryHC() {
+	// gracefull shutdown part start
 	hc.locker.Lock()
 	defer hc.locker.Unlock()
+	hc.gracefullShutdown.Lock()
+	if hc.gracefullShutdown.ShutdownNow {
+		defer hc.gracefullShutdown.Unlock()
+		hc.logging.WithFields(logrus.Fields{
+			"entity":     healthcheckName,
+			"event uuid": healthcheckUUID,
+		}).Warnf("program got shutdown signal, job heathcheck cancel")
+		return
+	}
+	hc.gracefullShutdown.UsecasesJobs++
+	hc.gracefullShutdown.Unlock()
+	defer decreaseJobs(hc.gracefullShutdown)
+	// gracefull shutdown part end
+
+	servicesInfo, err := hc.cacheStorage.LoadAllStorageDataToDomainModel()
+	if err != nil {
+		hc.logging.WithFields(logrus.Fields{
+			"entity":     healthcheckName,
+			"event uuid": healthcheckUUID,
+		}).Info("Fail to load storage data to services info for healthcheck")
+		go func(hc *HeathcheckEntity) {
+			time.Sleep(5 * time.Second)
+			hc.waitRetry <- struct{}{}
+		}(hc)
+		return
+	}
+	hc.CheckAllApplicationServersInServices(servicesInfo)
+}
+
+// CheckAllApplicationServersInServices ...
+func (hc *HeathcheckEntity) CheckAllApplicationServersInServices(servicesInfo []*domain.ServiceInfo) {
 	for _, serviceInfo := range servicesInfo {
 		hc.CheckApplicationServersInService(serviceInfo)
 	}
 }
 
 // CheckApplicationServersInService ...
-// TODO: maybe check application server and return err, wheb usecase create service/add app servers?
 func (hc *HeathcheckEntity) CheckApplicationServersInService(serviceInfo *domain.ServiceInfo) {
 	var failedServices int
 	for _, applicationServerInfo := range serviceInfo.ApplicationServers {
 		if hc.tcpCheckFail(applicationServerInfo.ServerIP, applicationServerInfo.ServerPort) {
 			failedServices++
 			if applicationServerInfo.State {
+				hc.logging.WithFields(logrus.Fields{
+					"entity":     healthcheckName,
+					"event uuid": healthcheckUUID,
+				}).Infof("is service %v application server %v is down",
+					serviceInfo.ServiceIP+":"+serviceInfo.ServicePort,
+					applicationServerInfo.ServerIP+":"+applicationServerInfo.ServerPort)
+				applicationServerInfo.State = false
 				hc.excludeApplicationServerFromIPVS(serviceInfo, applicationServerInfo)
 			}
-			applicationServerInfo.State = false
 			continue
 		}
 		if !applicationServerInfo.State {
+			hc.logging.WithFields(logrus.Fields{
+				"entity":     healthcheckName,
+				"event uuid": healthcheckUUID,
+			}).Infof("is service %v application server %v is up",
+				serviceInfo.ServiceIP+":"+serviceInfo.ServicePort,
+				applicationServerInfo.ServerIP+":"+applicationServerInfo.ServerPort)
+			applicationServerInfo.State = true
 			hc.inclideApplicationServerInIPVS(serviceInfo, applicationServerInfo)
+			continue
 		}
-		applicationServerInfo.State = true
 	}
 	if len(serviceInfo.ApplicationServers)-failedServices < 2 { // hardcode
 		serviceInfo.State = false
+		return
+	} else if !serviceInfo.State {
+		serviceInfo.State = true
 	}
-	serviceInfo.State = true
+	hc.updateInStorages(serviceInfo)
+}
+
+func (hc *HeathcheckEntity) updateInStorages(serviceInfo *domain.ServiceInfo) {
+	errUpdataCache := hc.cacheStorage.UpdateServiceInfo(serviceInfo, healthcheckUUID)
+	if errUpdataCache != nil {
+		hc.logging.WithFields(logrus.Fields{
+			"entity":     healthcheckName,
+			"event uuid": healthcheckUUID,
+		}).Errorf("Heathcheck update info in cache fail: %v", errUpdataCache)
+	}
+
+	errPersistantStorage := hc.persistentStorage.UpdateServiceInfo(serviceInfo, healthcheckUUID)
+	if errPersistantStorage != nil {
+		hc.logging.WithFields(logrus.Fields{
+			"entity":     healthcheckName,
+			"event uuid": healthcheckUUID,
+		}).Errorf("Heathcheck update info in persistent storage fail: %v", errPersistantStorage)
+	}
 }
 
 func (hc *HeathcheckEntity) tcpCheckFail(ip, port string) bool {
@@ -70,7 +163,7 @@ func (hc *HeathcheckEntity) tcpCheckFail(ip, port string) bool {
 		hc.logging.WithFields(logrus.Fields{
 			"entity":     healthcheckName,
 			"event uuid": healthcheckUUID,
-		}).Errorf("Heathcheck error: Connecting error: %v", err)
+		}).Debugf("Heathcheck error: Connecting error: %v", err)
 		return true
 	}
 	if conn != nil {
