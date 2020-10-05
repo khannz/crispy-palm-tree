e7feceba407cd7a202e1255c488e18a13f35e258
Ivan Tihonov <4thevan@gmail.com>
2020-10-05 18:26:00 +0300

    rework work with storage

--- portadapter/storage-badger.go
+++ portadapter/storage-badger.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"strings"
 	"sync"
-	"time"
 
 	badger "github.com/dgraph-io/badger/v2"
 	"github.com/khannz/crispy-palm-tree/domain"
@@ -55,41 +54,16 @@ func optionsForDbPersistent(dbPath string, logger *logrus.Logger) badger.Options
 	return defOpt
 }
 
-// ExtendedServiceData have application servers info and info abou service
-type ExtendedServiceData struct {
-	ServiceRepeatHealthcheck                          time.Duration              `json:"serviceRepeatHealthcheck"`
-	ServicePercentOfAlivedForUp                       int                        `json:"servicePercentOfAlivedForUp"`
-	ServiceHealthcheckType                            string                     `json:"serviceHealthcheckType"`
-	ServiceHealthcheckTimeout                         time.Duration              `json:"serviceHealthcheckTimeout"`
-	ServiceHealthcheckRetriesForUpApplicationServer   int                        `json:"serviceHealthcheckRetriesForUpApplicationServer"`
-	ServiceHealthcheckRetriesForDownApplicationServer int                        `json:"serviceHealthcheckRetriesForDownApplicationServer"`
-	ServiceExtraInfo                                  []string                   `json:"serviceExtraInfo"`
-	ServiceIsUp                                       bool                       `json:"serviceIsUp"`
-	ApplicationServers                                []domain.ApplicationServer `json:"applicationServers"`
-	BalanceType                                       string                     `json:"balanceType"`
-	RoutingType                                       string                     `json:"routingType"`
-	Protocol                                          string                     `json:"protocol"`
-}
-
-// TunnelForService ...
-type TunnelForService struct {
-	SysctlConfFile        string `json:"sysctlConf"` // full path to sysctl conf file
-	TunnelName            string `json:"tunnelName"`
-	ServicesToTunnelCount int    `json:"servicesToTunnelCount"`
-}
-
 // NewServiceInfoToStorage add new service to storage
 func (storageEntity *StorageEntity) NewServiceInfoToStorage(serviceData *domain.ServiceInfo,
 	eventID string) error {
-	serviceDataKey, serviceDataValue, err := transformServiceDataForStorageData(serviceData)
+	serviceDataKey := []byte(serviceData.Address)
+	serviceDataValue, err := json.Marshal(serviceData)
 	if err != nil {
-		return fmt.Errorf("can't form data for storage: %v", err)
+		return fmt.Errorf("can't marshal service data: %v", err)
 	}
 
-	storageEntity.Lock()
-	defer storageEntity.Unlock()
-
-	err = storageEntity.updateDatabaseServiceInfo(serviceDataKey, serviceDataValue)
+	err = updateDb(storageEntity.Db, serviceDataKey, serviceDataValue)
 	if err != nil {
 		return fmt.Errorf("can't update storage for new service: %v", err)
 	}
@@ -97,57 +71,18 @@ func (storageEntity *StorageEntity) NewServiceInfoToStorage(serviceData *domain.
 	return nil
 }
 
-func transformServiceDataForStorageData(serviceData *domain.ServiceInfo) ([]byte,
-	[]byte,
-	error) {
-	serviceDataKey := []byte(serviceData.IP + ":" + serviceData.Port)
-
-	renewApplicationServers := []domain.ApplicationServer{}
-	for _, applicationServer := range serviceData.ApplicationServers {
-		renewApplicationServer := *applicationServer
-		renewApplicationServers = append(renewApplicationServers, renewApplicationServer)
-	}
-
-	transformedServiceData := ExtendedServiceData{
-		ServiceRepeatHealthcheck:                          serviceData.Healthcheck.RepeatHealthcheck,
-		ServicePercentOfAlivedForUp:                       serviceData.Healthcheck.PercentOfAlivedForUp,
-		ServiceHealthcheckType:                            serviceData.Healthcheck.Type,
-		ServiceHealthcheckTimeout:                         serviceData.Healthcheck.Timeout,
-		ServiceHealthcheckRetriesForUpApplicationServer:   serviceData.Healthcheck.RetriesForUpApplicationServer,
-		ServiceHealthcheckRetriesForDownApplicationServer: serviceData.Healthcheck.RetriesForDownApplicationServer,
-		ServiceExtraInfo:                                  serviceData.ExtraInfo,
-		ServiceIsUp:                                       serviceData.IsUp,
-		ApplicationServers:                                renewApplicationServers,
-		BalanceType:                                       serviceData.BalanceType,
-		RoutingType:                                       serviceData.RoutingType,
-		Protocol:                                          serviceData.Protocol,
-	}
-	serviceDataValue, err := json.Marshal(transformedServiceData)
-	if err != nil {
-		return nil, nil, fmt.Errorf("can't marshal transformedServiceData: %v", err)
-	}
-	return serviceDataKey, serviceDataValue, nil
-}
-
 func updateDb(db *badger.DB, key, value []byte) error {
+	db.Lock()
+	defer db.Unlock()
 	return db.Update(func(txn *badger.Txn) error {
 		err := txn.Set(key, value)
 		return err
 	})
 }
 
-func (storageEntity *StorageEntity) updateDatabaseServiceInfo(serviceDataKey,
-	serviceDataValue []byte) error {
-	err := updateDb(storageEntity.Db, serviceDataKey, serviceDataValue)
-	if err != nil {
-		return fmt.Errorf("can't update db for service data: %v", err)
-	}
-	return nil
-}
-
 // RemoveServiceInfoFromStorage ...
 func (storageEntity *StorageEntity) RemoveServiceInfoFromStorage(serviceData *domain.ServiceInfo, eventID string) error {
-	keyData := []byte(serviceData.IP + ":" + serviceData.Port)
+	keyData := []byte(serviceData.Address)
 	storageEntity.Lock()
 	defer storageEntity.Unlock()
 
@@ -163,81 +98,30 @@ func (storageEntity *StorageEntity) RemoveServiceInfoFromStorage(serviceData *do
 }
 
 // GetServiceInfo ...
-func (storageEntity *StorageEntity) GetServiceInfo(incomeServiceData *domain.ServiceInfo, eventID string) (*domain.ServiceInfo, error) {
-	shc := domain.ServiceHealthcheck{
-		RepeatHealthcheck:               3000000009,
-		Type:                            "",
-		Timeout:                         time.Duration(999 * time.Second),
-		RetriesForUpApplicationServer:   0,
-		RetriesForDownApplicationServer: 0,
-	}
-	currentServiceInfo := &domain.ServiceInfo{
-		ServiceIP:          "",
-		ServicePort:        "",
-		ApplicationServers: []*domain.ApplicationServer{},
-		Healthcheck:        shc,
-		ExtraInfo:          []string{},
-		IsUp:               false,
-		BalanceType:        "",
-		RoutingType:        "",
-		Protocol:           "",
-	}
-	currentApplicationServers := []*domain.ApplicationServer{}
+func (storageEntity *StorageEntity) GetServiceInfo(serviceData *domain.ServiceInfo, eventID string) (*domain.ServiceInfo, error) {
 	storageEntity.Lock()
 	defer storageEntity.Unlock()
+	dbServiceData := &domain.ServiceInfo{}
 	if err := storageEntity.Db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte(incomeServiceData.IP + ":" + incomeServiceData.Port))
+		item, err := txn.Get([]byte(serviceData.Address))
 		if err != nil {
 			return fmt.Errorf("txn.Get fail: %v", err)
 		}
 
-		oldExtendedServiceData := ExtendedServiceData{}
 		if err = item.Value(func(val []byte) error {
-			if err := json.Unmarshal(val, &oldExtendedServiceData); err != nil {
+			if err := json.Unmarshal(val, &dbServiceData); err != nil {
 				return fmt.Errorf("can't unmarshall application servers data: %v", err)
 			}
 			return nil
 		}); err != nil {
 			return err
 		}
-		for _, oldApplicationServer := range oldExtendedServiceData.ApplicationServers {
-			renewPointerForOldApplicationServer := oldApplicationServer
-			currentApplicationServers = append(currentApplicationServers, &renewPointerForOldApplicationServer)
-		}
-
-		currentServiceInfo.Healthcheck = domain.ServiceHealthcheck{
-			RepeatHealthcheck:               oldExtendedServiceData.ServiceRepeatHealthcheck,
-			PercentOfAlivedForUp:            oldExtendedServiceData.ServicePercentOfAlivedForUp,
-			Type:                            oldExtendedServiceData.ServiceHealthcheckType,
-			Timeout:                         oldExtendedServiceData.ServiceHealthcheckTimeout,
-			RetriesForUpApplicationServer:   oldExtendedServiceData.ServiceHealthcheckRetriesForUpApplicationServer,
-			RetriesForDownApplicationServer: oldExtendedServiceData.ServiceHealthcheckRetriesForDownApplicationServer,
-		}
-
-		if oldExtendedServiceData.ServiceExtraInfo != nil {
-			currentServiceInfo.ExtraInfo = oldExtendedServiceData.ServiceExtraInfo
-		}
-		currentServiceInfo.IsUp = oldExtendedServiceData.ServiceIsUp
-		tmpBalanceType := oldExtendedServiceData.BalanceType
-		tmpRoutingType := oldExtendedServiceData.RoutingType
-		tmpProtocol := oldExtendedServiceData.Protocol
-		currentServiceInfo.BalanceType = tmpBalanceType
-		currentServiceInfo.RoutingType = tmpRoutingType
-		currentServiceInfo.Protocol = tmpProtocol
-
 		return nil
 	}); err != nil {
-		return currentServiceInfo, err
+		return nil, err
 	}
 
-	tmpIncomeServiceIP := incomeServiceData.IP
-	tmpIncomeServicePort := incomeServiceData.Port
-
-	currentServiceInfo.IP = tmpIncomeServiceIP
-	currentServiceInfo.Port = tmpIncomeServicePort
-	currentServiceInfo.ApplicationServers = currentApplicationServers
-
-	return currentServiceInfo, nil
+	return dbServiceData, nil
 }
 
 // LoadAllStorageDataToDomainModels ...
@@ -254,42 +138,16 @@ func (storageEntity *StorageEntity) LoadAllStorageDataToDomainModels() ([]*domai
 			item := it.Item()
 			key := item.Key()
 			err := item.Value(func(val []byte) error {
-				oldExtendedServiceData := ExtendedServiceData{}
-				if err := json.Unmarshal(val, &oldExtendedServiceData); err != nil {
+				dbServiceData := &domain.ServiceInfo{}
+				if err := json.Unmarshal(val, &dbServiceData); err != nil {
 					return fmt.Errorf("can't unmarshall application servers data: %v", err)
 				}
 				rawServiceData := strings.Split(string(key), ":")
 				if len(rawServiceData) != 2 {
-					return nil
-					// return fmt.Errorf("fail when take service data, expect format x.x.x.x:p, have: %s", key)
-				}
-				currentApplicationServers := []*domain.ApplicationServer{}
-				for _, oldApplicationServer := range oldExtendedServiceData.ApplicationServers {
-					renewPointerForOldApplicationServer := oldApplicationServer
-					currentApplicationServers = append(currentApplicationServers, &renewPointerForOldApplicationServer)
+					return nil // it's not service info
 				}
 
-				hc := domain.ServiceHealthcheck{
-					RepeatHealthcheck:               oldExtendedServiceData.ServiceRepeatHealthcheck,
-					PercentOfAlivedForUp:            oldExtendedServiceData.ServicePercentOfAlivedForUp,
-					Type:                            oldExtendedServiceData.ServiceHealthcheckType,
-					Timeout:                         oldExtendedServiceData.ServiceHealthcheckTimeout,
-					RetriesForUpApplicationServer:   oldExtendedServiceData.ServiceHealthcheckRetriesForUpApplicationServer,
-					RetriesForDownApplicationServer: oldExtendedServiceData.ServiceHealthcheckRetriesForDownApplicationServer,
-				}
-
-				serviceInfo := &domain.ServiceInfo{
-					ServiceIP:          rawServiceData[0],
-					ServicePort:        rawServiceData[1],
-					ApplicationServers: currentApplicationServers,
-					Healthcheck:        hc,
-					ExtraInfo:          oldExtendedServiceData.ServiceExtraInfo,
-					IsUp:               oldExtendedServiceData.ServiceIsUp,
-					BalanceType:        oldExtendedServiceData.BalanceType,
-					RoutingType:        oldExtendedServiceData.RoutingType,
-					Protocol:           oldExtendedServiceData.Protocol,
-				}
-				servicesInfo = append(servicesInfo, serviceInfo)
+				servicesInfo = append(servicesInfo, dbServiceData)
 				return nil
 			})
 			if err != nil {
@@ -335,14 +193,14 @@ func (storageEntity *StorageEntity) LoadCacheFromStorage(oldStorageEntity *Stora
 }
 
 // UpdateServiceInfo validate and update service
-func (storageEntity *StorageEntity) UpdateServiceInfo(newServiceData *domain.ServiceInfo, eventID string) error {
-	serviceDataKey, serviceDataValue, err := transformServiceDataForStorageData(newServiceData)
+func (storageEntity *StorageEntity) UpdateServiceInfo(serviceData *domain.ServiceInfo, eventID string) error {
+	serviceDataKey := []byte(serviceData.Address)
+	serviceDataValue, err := json.Marshal(serviceData)
 	if err != nil {
-		return fmt.Errorf("can't form data for storage: %v", err)
+		return fmt.Errorf("can't marshal service data: %v", err)
 	}
-	storageEntity.Lock()
-	defer storageEntity.Unlock()
-	err = storageEntity.updateDatabaseServiceInfo(serviceDataKey, serviceDataValue)
+
+	err = updateDb(storageEntity.Db, serviceDataKey, serviceDataValue)
 	if err != nil {
 		return fmt.Errorf("can't update storage for new service: %v", err)
 	}
@@ -352,7 +210,7 @@ func (storageEntity *StorageEntity) UpdateServiceInfo(newServiceData *domain.Ser
 
 // ReadTunnelInfoForApplicationServer return nil, if key not exist
 func (storageEntity *StorageEntity) ReadTunnelInfoForApplicationServer(ip string) *domain.TunnelForApplicationServer {
-	dTunnelInfo := &domain.TunnelForApplicationServer{}
+	tunnelInfo := &domain.TunnelForApplicationServer{}
 	storageEntity.Lock()
 	defer storageEntity.Unlock()
 	if err := storageEntity.Db.View(func(txn *badger.Txn) error {
@@ -360,29 +218,20 @@ func (storageEntity *StorageEntity) ReadTunnelInfoForApplicationServer(ip string
 		if err != nil {
 			return fmt.Errorf("txn.Get fail: %v", err)
 		}
-		sTunnelInfo := &TunnelForService{}
+		tunnelInfo := &domain.TunnelForApplicationServer{}
 		if err = item.Value(func(val []byte) error {
-			if err := json.Unmarshal(val, &sTunnelInfo); err != nil {
+			if err := json.Unmarshal(val, &tunnelInfo); err != nil {
 				return fmt.Errorf("can't unmarshall tunnnel data: %v", err)
 			}
 			return nil
 		}); err != nil {
 			return err
 		}
-		if sTunnelInfo.ServicesToTunnelCount == 0 {
-			return fmt.Errorf("no tunnel files: %v", sTunnelInfo.ServicesToTunnelCount)
-		}
-
-		dTunnelInfo.ApplicationServerIP = ip
-		dTunnelInfo.ServicesToTunnelCount = sTunnelInfo.ServicesToTunnelCount
-		dTunnelInfo.SysctlConfFile = sTunnelInfo.SysctlConfFile
-		dTunnelInfo.TunnelName = sTunnelInfo.TunnelName
-
 		return nil
 	}); err != nil {
 		return nil
 	}
-	return dTunnelInfo
+	return tunnelInfo
 }
 
 // GetAllApplicationServersWhoHaveTunnels bad code :(
@@ -457,16 +306,11 @@ func (storageEntity *StorageEntity) UpdateTunnelFilesInfoAtStorage(tunnelsFilesI
 	defer storageEntity.Unlock()
 	for _, tunnelFilesInfo := range tunnelsFilesInfo {
 		key := []byte(tunnelFilesInfo.ApplicationServerIP)
-		transformedTunnelForService := TunnelForService{
-			SysctlConfFile:        tunnelFilesInfo.SysctlConfFile,
-			TunnelName:            tunnelFilesInfo.TunnelName,
-			ServicesToTunnelCount: tunnelFilesInfo.ServicesToTunnelCount,
-		}
-		tunnelsFilesInfoValue, err := json.Marshal(transformedTunnelForService)
+		tunnelFileInfoValue, err := json.Marshal(tunnelsFilesInfo)
 		if err != nil {
 			return fmt.Errorf("can't marshal transformedTunnelForService: %v", err)
 		}
-		if err := updateDb(storageEntity.Db, key, tunnelsFilesInfoValue); err != nil {
+		if err := updateDb(storageEntity.Db, key, tunnelFileInfoValue); err != nil {
 			return fmt.Errorf("can't update db: %v", err)
 		}
 	}
