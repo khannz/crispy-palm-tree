5c1d71f3eb9470001745eb41e6d6a277cb086712
Ivan Tihonov <4thevan@gmail.com>
2020-11-17 09:07:53 +0300

    change grpc tcp to uds

--- t1-ipvs/portadapter/hc-client.go
+++ t1-ipvs/portadapter/hc-client.go
@@ -3,6 +3,7 @@ package portadapter
 import (
 	"context"
 	"fmt"
+	"net"
 	"time"
 
 	transport "github.com/khannz/crispy-palm-tree/lbost1a-ipvs/grpc-transport"
@@ -28,21 +29,36 @@ func NewHealthcheckWorkerEntity(address string, grpcTimeout time.Duration, loggi
 
 func (healthcheckWorker *HealthcheckWorkerEntity) SendIPVSRuntime(runtimeConfig map[string]map[string]uint16,
 	id string) error {
+	withContextDialer := makeDialer(healthcheckWorker.address, 2*time.Second)
+
 	dialCtx, dialCancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer dialCancel()
-	conn, err := grpc.DialContext(dialCtx, healthcheckWorker.address, grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.DialContext(dialCtx, healthcheckWorker.address, grpc.WithInsecure(), grpc.WithContextDialer(withContextDialer))
 	if err != nil {
-		return fmt.Errorf("did not connect to grpc server: %v", err)
+		return fmt.Errorf("can't connect to grpc uds server: %v", err)
 	}
 	defer conn.Close()
-	healthcheckClient := transport.NewSendIPVSRuntimeClient(conn)
 
+	healthcheckClient := transport.NewSendIPVSRuntimeClient(conn)
 	sendCtx, sendCancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer sendCancel()
 
 	pbRuntime := convertInternalServicesToPbServices(runtimeConfig, id)
 	_, err = healthcheckClient.SendIPVSRuntime(sendCtx, pbRuntime)
 	return err
+}
+
+func makeDialer(addr string, t time.Duration) func(ctx context.Context, addr string) (net.Conn, error) {
+	f := func(addr string, t time.Duration) (net.Conn, error) {
+		return net.Dial("unix", addr)
+	}
+
+	return func(ctx context.Context, addr string) (net.Conn, error) {
+		if deadline, ok := ctx.Deadline(); ok {
+			return f(addr, time.Until(deadline))
+		}
+		return f(addr, 0)
+	}
 }
 
 func convertInternalToPbApplicationServers(internalApplicationServers map[string]uint16) map[string]uint32 {
