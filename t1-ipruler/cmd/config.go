813f3dfb2f4a315610eb443d2690cabdfe1962ec
thevan <4thevan@gmail.com>
2021-01-28 15:41:13 +0300

    refactor ipruler config

--- t1-ipruler/cmd/config.go
+++ t1-ipruler/cmd/config.go
@@ -7,48 +7,15 @@ import (
 	"time"
 
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	logger "github.com/thevan4/logrus-wrapper"
 )
 
-// Default values
-const (
-	defaultConfigFilePath   = "./lbost1aipr.yaml"
-	defaultLogOutput        = "stdout"
-	defaultLogLevel         = "trace"
-	defaultLogFormat        = "text"
-	defaultSystemLogTag     = ""
-	defaultLogEventLocation = true
+var cfgFile string
 
-	defaultIDType = "nanoid"
-
-	defaultHCAddress = "/var/run/lbost1ao.sock"
-	defaultHCTimeout = 2 * time.Second
-
-	defaultRouteAddress = "/var/run/lbost1aipr.sock"
-	defaultRouteTimeout = 2 * time.Second
-)
-
-// Config names
-const (
-	configFilePathName   = "config-file-path"
-	logOutputName        = "log-output"
-	logLevelName         = "log-level"
-	logFormatName        = "log-format"
-	syslogTagName        = "syslog-tag"
-	logEventLocationName = "log-event-location"
-
-	hcAddressName = "hc-address"
-	hcTimeoutName = "hc-timeout"
-
-	ipRuleAddressName = "ipRule-address"
-	ipRuleTimeoutName = "ipRule-timeout"
-
-	idTypeName = "id-type"
-)
-
-// // For builds with ldflags
+// For builds with ldflags
 var (
 	version   = "unknown"
 	buildTime = "unknown"
@@ -57,62 +24,102 @@ var (
 )
 
 var (
-	viperConfig *viper.Viper
-	logging     *logrus.Logger
+	logging *logrus.Logger
 )
 
 func init() {
-	var err error
-	viperConfig = viper.New()
-	// work with env
-	viperConfig.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-	viperConfig.AutomaticEnv()
+	cobra.OnInitialize(initConfig)
 
-	// work with flags
-	pflag.StringP(configFilePathName, "c", defaultConfigFilePath, "Path to config file. Example value: './nw-lb.yaml'")
-	pflag.String(logOutputName, defaultLogOutput, "Log output. Example values: 'stdout', 'syslog'")
-	pflag.String(logLevelName, defaultLogLevel, "Log level. Example values: 'info', 'debug', 'trace'")
-	pflag.String(logFormatName, defaultLogFormat, "Log format. Example values: 'text', 'json'")
-	pflag.String(syslogTagName, defaultSystemLogTag, "Syslog tag. Example: 'trac-dgen'")
-	pflag.Bool(logEventLocationName, defaultLogEventLocation, "Log event location (like python)")
+	pflag.StringVarP(&cfgFile,
+		"config-file-path",
+		"c",
+		"./lbost1aipr.yaml",
+		"Path to config file. Example value: './nw-lb.yaml'")
+	pflag.String("log-output",
+		"stdout",
+		"Log output. Example values: 'stdout', 'syslog'")
+	pflag.String("log-level",
+		"info",
+		"Log level. Example values: 'info', 'debug', 'trace'")
+	pflag.String("log-format",
+		"text",
+		"Log format. Example values: 'text', 'json'")
+	pflag.String("syslog-tag",
+		"",
+		"Syslog tag. Example: 'trac-dgen'")
+	pflag.Bool("log-event-location",
+		true,
+		"Log event location (like python)")
 
-	pflag.String(hcAddressName, defaultHCAddress, "hc address")
-	pflag.Duration(hcTimeoutName, defaultHCTimeout, "hc timeout")
+	pflag.String("orch-address",
+		"/var/run/lbost1ao.sock",
+		"hc address")
+	pflag.Duration("orch-timeout",
+		2*time.Second,
+		"hc timeout")
 
-	pflag.String(ipRuleAddressName, defaultRouteAddress, "ipRule address")
-	pflag.Duration(ipRuleTimeoutName, defaultRouteTimeout, "ipRule address")
+	pflag.String("ipRule-address",
+		"/var/run/lbost1aipr.sock",
+		"ipRule address")
+	pflag.Duration("ipRule-timeout",
+		2*time.Second,
+		"ipRule address")
 
-	pflag.String(idTypeName, defaultIDType, "ID type(nanoid|uuid4)")
+	pflag.String("id-type",
+		"nanoid",
+		"ID type(nanoid|uuid4)")
 
-	pflag.Parse()
-	if err := viperConfig.BindPFlags(pflag.CommandLine); err != nil {
-		fmt.Println(err)
+	if err := viper.BindPFlags(rootCmd.PersistentFlags()); err != nil {
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	// work with config file
-	viperConfig.SetConfigFile(viperConfig.GetString(configFilePathName))
-	if err := viperConfig.ReadInConfig(); err != nil {
-		fmt.Println(err)
+	initLogger()
+	validateValues()
+
+	rootCmd.AddCommand(runCmd)
+
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
 	}
 
-	// init logs
-	newLogger := &logger.Logger{
-		Output:           []string{viperConfig.GetString(logOutputName)},
-		Level:            viperConfig.GetString(logLevelName),
-		Formatter:        viperConfig.GetString(logFormatName),
-		LogEventLocation: viperConfig.GetBool(logEventLocationName),
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("can't read config from file, error:", err)
 	}
+}
+
+func initLogger() {
+	newLogger := &logger.Logger{
+		Output:           []string{viper.GetString("log-output")},
+		Level:            viper.GetString("log-level"),
+		Formatter:        viper.GetString("log-format"),
+		LogEventLocation: viper.GetBool("log-event-location"),
+	}
+	var err error
 	logging, err = logger.NewLogrusLogger(newLogger)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("init log error:", err)
 		os.Exit(1)
 	}
+}
 
-	switch viperConfig.GetString(idTypeName) {
+func validateValues() {
+	// required values are set
+	if viper.GetString("dummy-interface") == "" {
+		logging.Fatalf("tech interface must be set")
+	}
+
+	switch viper.GetString("id-type") {
 	case "nanoid":
 	case "id4":
 	default:
-		logging.Fatalf("unsuported id type: %v; supported types: nanoid|id4", viperConfig.GetString(idTypeName))
+		logging.Fatalf("unsuported id type: %v; supported types: nanoid|id4", viper.GetString("id-type"))
 	}
 }
--- t1-ipruler/cmd/run.go
+++ t1-ipruler/cmd/run.go
@@ -11,13 +11,18 @@ import (
 	"github.com/khannz/crispy-palm-tree/t1-ipruler/portadapter"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "run",
-	Short: "lbost1aipr 😉",
+	Use:   "lbost1ad",
+	Short: "ip rule customizer ;-)",
+}
+
+var runCmd = &cobra.Command{
+	Use: "run",
 	Run: func(cmd *cobra.Command, args []string) {
-		idGenerator := chooseIDGenerator(viperConfig.GetString(idTypeName))
+		idGenerator := chooseIDGenerator()
 		idForRootProcess := idGenerator.NewID()
 
 		// validate fields
@@ -25,17 +30,17 @@ var rootCmd = &cobra.Command{
 			"version":          version,
 			"build time":       buildTime,
 			"event id":         idForRootProcess,
-			"config file path": viperConfig.GetString(configFilePathName),
-			"log format":       viperConfig.GetString(logFormatName),
-			"log level":        viperConfig.GetString(logLevelName),
-			"log output":       viperConfig.GetString(logOutputName),
-			"syslog tag":       viperConfig.GetString(syslogTagName),
+			"config file path": viper.GetString("config-file-path"),
+			"log format":       viper.GetString("log-format"),
+			"log level":        viper.GetString("log-level"),
+			"log output":       viper.GetString("log-output"),
+			"syslog tag":       viper.GetString("syslog-tag"),
 
-			"hc address":     viperConfig.GetString(hcAddressName),
-			"hc timeout":     viperConfig.GetDuration(hcTimeoutName),
-			"ipRule address": viperConfig.GetString(ipRuleAddressName),
-			"ipRule timeout": viperConfig.GetDuration(ipRuleTimeoutName),
-			"id type":        viperConfig.GetString(idTypeName),
+			"hc address":     viper.GetString("orch-address"),
+			"hc timeout":     viper.GetDuration("orch-timeout"),
+			"ipRule address": viper.GetString("ipRule-address"),
+			"ipRule timeout": viper.GetDuration("ipRule-timeout"),
+			"id type":        viper.GetString("id-type"),
 		}).Info("")
 
 		// more about signals: https://en.wikipedia.org/wiki/Signal_(IPC)
@@ -50,8 +55,8 @@ var rootCmd = &cobra.Command{
 		// ipRuleConfigurator end
 
 		// OrchestratorWorker start
-		hw := portadapter.NewOrchestratorWorkerEntity(viperConfig.GetString(hcAddressName),
-			viperConfig.GetDuration(hcTimeoutName),
+		hw := portadapter.NewOrchestratorWorkerEntity(viper.GetString("orch-address"),
+			viper.GetDuration("orch-timeout"),
 			logging)
 		// OrchestratorWorker end
 
@@ -67,7 +72,7 @@ var rootCmd = &cobra.Command{
 		go facade.TryToSendRuntimeConfig(idForSendRuntimeConfig)
 
 		// up grpc api
-		grpcServer := application.NewGrpcServer(viperConfig.GetString(ipRuleAddressName), facade, logging) // gorutine inside
+		grpcServer := application.NewGrpcServer(viper.GetString("ipRule-address"), facade, logging) // gorutine inside
 		if err := grpcServer.StartServer(); err != nil {
 			logging.WithFields(logrus.Fields{"event id": idForRootProcess}).Fatalf("grpc server start error: %v", err)
 		}
@@ -91,8 +96,8 @@ func Execute() {
 	}
 }
 
-func chooseIDGenerator(idType string) domain.IDgenerator {
-	switch viperConfig.GetString(idTypeName) {
+func chooseIDGenerator() domain.IDgenerator {
+	switch viper.GetString("id-type") {
 	case "nanoid":
 		return portadapter.NewIDGenerator()
 	case "uuid4":
