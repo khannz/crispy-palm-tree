23002f78fd714335b48399cd9a19b8dcaf8daea8
thevan <4thevan@gmail.com>
2021-01-28 16:24:39 +0300

    refactor route config

--- t1-route/cmd/run.go
+++ t1-route/cmd/run.go
@@ -11,13 +11,18 @@ import (
 	"github.com/khannz/crispy-palm-tree/t1-route/portadapter"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "run",
-	Short: "lbost1ar 😉",
+	Use:   "lbost1ar",
+	Short: "route customizer ;-)",
+}
+
+var runCmd = &cobra.Command{
+	Use: "run",
 	Run: func(cmd *cobra.Command, args []string) {
-		idGenerator := chooseIDGenerator(viperConfig.GetString(idTypeName))
+		idGenerator := chooseIDGenerator()
 		idForRootProcess := idGenerator.NewID()
 
 		// validate fields
@@ -25,17 +30,17 @@ var rootCmd = &cobra.Command{
 			"version":          version,
 			"build time":       buildTime,
 			"event id":         idForRootProcess,
-			"config file path": viperConfig.GetString(configFilePathName),
-			"log format":       viperConfig.GetString(logFormatName),
-			"log level":        viperConfig.GetString(logLevelName),
-			"log output":       viperConfig.GetString(logOutputName),
-			"syslog tag":       viperConfig.GetString(syslogTagName),
+			"config file path": viper.GetString("config-file-path"),
+			"log format":       viper.GetString("log-format"),
+			"log level":        viper.GetString("log-level"),
+			"log output":       viper.GetString("log-output"),
+			"syslog tag":       viper.GetString("syslog-tag"),
 
-			"orch address":  viperConfig.GetString(orchAddressName),
-			"orch timeout":  viperConfig.GetDuration(orchTimeoutName),
-			"route address": viperConfig.GetString(routeAddressName),
-			"route timeout": viperConfig.GetDuration(routeTimeoutName),
-			"id type":       viperConfig.GetString(idTypeName),
+			"orch address":  viper.GetString("orch-address"),
+			"orch timeout":  viper.GetDuration("orch-timeout"),
+			"route address": viper.GetString("route-address"),
+			"route timeout": viper.GetDuration("route-timeout"),
+			"id type":       viper.GetString("id-type"),
 		}).Info("")
 
 		// more about signals: https://en.wikipedia.org/wiki/Signal_(IPC)
@@ -50,8 +55,8 @@ var rootCmd = &cobra.Command{
 		// routeConfigurator end
 
 		// OrchestratorWorker start
-		orch := portadapter.NewOrchestratorWorkerEntity(viperConfig.GetString(orchAddressName),
-			viperConfig.GetDuration(orchTimeoutName),
+		orch := portadapter.NewOrchestratorWorkerEntity(viper.GetString("orch-address"),
+			viper.GetDuration("orch-timeout"),
 			logging)
 		// OrchestratorWorker end
 
@@ -67,7 +72,7 @@ var rootCmd = &cobra.Command{
 		go facade.TryToSendRuntimeConfig(idForSendRuntimeConfig)
 
 		// up grpc api
-		grpcServer := application.NewGrpcServer(viperConfig.GetString(routeAddressName), facade, logging) // gorutine inside
+		grpcServer := application.NewGrpcServer(viper.GetString("route-address"), facade, logging) // gorutine inside
 		if err := grpcServer.StartServer(); err != nil {
 			logging.WithFields(logrus.Fields{"event id": idForRootProcess}).Fatalf("grpc server start error: %v", err)
 		}
@@ -91,8 +96,8 @@ func Execute() {
 	}
 }
 
-func chooseIDGenerator(idType string) domain.IDgenerator {
-	switch viperConfig.GetString(idTypeName) {
+func chooseIDGenerator() domain.IDgenerator {
+	switch viper.GetString("id-type") {
 	case "nanoid":
 		return portadapter.NewIDGenerator()
 	case "uuid4":
