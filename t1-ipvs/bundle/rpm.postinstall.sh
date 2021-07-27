47c1c5bc37d89491bf668fdcf6528a3ef71fd891
Pavel Fiskovich <pavel.fiskovich@gmail.com>
2021-07-27 09:56:10 +0300

    Fixed RPM scripts

--- t1-ipvs/bundle/rpm.postinstall.sh
+++ t1-ipvs/bundle/rpm.postinstall.sh
@@ -1,9 +1,2 @@
-lbost1ai="lbost1ai"
-
-mkdir -p "/var/run/$lbost1ai"
-
 systemctl daemon-reload
-systemctl enable lbost1ai.service
-# systemctl start lbost1ai.service
-
-exit 0
+systemctl enable --now lbost1ai.service
