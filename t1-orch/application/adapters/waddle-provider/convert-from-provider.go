c7c4a70c10862f6545811b40ef1b085e6854728a
Pavel Fiskovich <pavel.fiskovich@gmail.com>
2021-12-13 00:33:17 +0300

    CRISPY-87: added protos-v1.1.1 parse logic

--- t1-orch/application/adapters/waddle-provider/convert-from-provider.go
+++ t1-orch/application/adapters/waddle-provider/convert-from-provider.go
@@ -1,9 +1,11 @@
 package waddle_provider
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/khannz/crispy-palm-tree/t1-orch/domain"
 	"github.com/khannz/crispy-palm-tree/t1-orch/providers/waddle"
-	"time"
 )
 
 type (
@@ -12,14 +14,23 @@ type (
 
 func (from ServiceConfigData) ToServiceInfoConf() (domain.ServiceInfoConf, error) {
 	services := from.GetServices()
+
 	servicesInfo := make(domain.ServiceInfoConf, len(services))
 	for _, service := range services {
+		r := map[string]*domain.ApplicationServer{}
+		srv := &domain.ServiceInfo{}
 
-		_ = service.GetProtocol()
-		_ = service.GetAddress()
-		_ = service.GetPort()
-		_ = service.GetRoutingType()
-		_ = service.GetBalancingType()
+		// TODO: no idea what is purpose of Address
+		srv.Address = fmt.Sprintf("%s:%d", service.GetAddress(), service.GetPort())
+
+		srv.Protocol = service.GetProtocol().String()
+		srv.IP = service.GetAddress()
+		srv.Port = fmt.Sprintf("%d", service.GetPort())
+
+		srv.BalanceType = service.GetBalancingType().String()
+		srv.RoutingType = service.GetRoutingType().String()
+
+		// TODO: srv.HealthcheckType = service.Get
 
 		for _, anReal := range service.GetReals() {
 			/*//
@@ -28,23 +39,31 @@ func (from ServiceConfigData) ToServiceInfoConf() (domain.ServiceInfoConf, error
 				applicationServers domain.ApplicationServers
 			)
 			*/
-			_ = anReal.GetAddress()
-			_ = anReal.GetPort()
 
+			/*
+				FIXME: until migration to protos-v2.* this logic with overwriting is acceptable
+				since following values in protos-v1.* should be shared for every Real
+			*/
 			hc := anReal.GetHealthcheck()
-			quorum := int(hc.GetQuorum())
-			helloTimer := time.Duration(hc.GetHelloTimer())
-			responseTimer := time.Duration(hc.GetResponseTimer())
-			aliveThreshold := int(hc.GetAliveThreshold())
-			deadThreshold := int(hc.GetDeadThreshold())
+			srv.HelloTimer = time.Duration(hc.GetHelloTimer()) * time.Millisecond
+			srv.ResponseTimer = time.Duration(hc.GetResponseTimer()) * time.Millisecond
+			srv.AliveThreshold = int(hc.AliveThreshold)
+			srv.DeadThreshold = int(hc.DeadThreshold)
+			srv.HealthcheckType = hc.Type
+			//srv.Uri = hc.Uri // TODO: does it implemented in core logic
+			//srv.ValidResponseCodes = hc.ResponseCodes // TODO: does it implemented in core logic
 
-			_ = quorum
-			_ = helloTimer
-			_ = responseTimer
-			_ = aliveThreshold
-			_ = deadThreshold
+			res := &domain.ApplicationServer{
+				Address:            fmt.Sprintf("%s:%d", anReal.GetAddress(), anReal.GetPort()),
+				IP:                 anReal.GetAddress(),
+				Port:               fmt.Sprintf("%d", anReal.GetPort()),
+				HealthcheckAddress: fmt.Sprintf("%s:%d", hc.Address, hc.Port),
+			}
 
+			r[res.Address] = res
 		}
+		srv.ApplicationServers = r
+		servicesInfo[srv.Address] = srv
 	}
 	return servicesInfo, nil
 }
